9875164861111e0c0ad0383a8b2af47965fb0a8b
HadssanK <hassankhan847754@gmail.com>
2025-12-16 23:39:03 +0500

    latest updated

--- app/customization/[slug]/CustomDetails.jsx
+++ app/customization/[slug]/CustomDetails.jsx
@@ -1,12 +1,10 @@
-"use client";
 
-import { Suspense } from "react";
 import CustomDetails from "./page";
 
 export default function CustomDetailsWrapper(props) {
   return (
-    <Suspense fallback={<div className="p-10 text-white">Loading...</div>}>
+    // <Suspense fallback={<div className="p-10 text-white">Loading...</div>}>
       <CustomDetails {...props} />
-    </Suspense>
+    // </Suspense>
   );
 }
